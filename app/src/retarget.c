ea52cc5c65cbe02cd94d371c6e296f78a28121b4
zincVTD <thanhdanhldt@gmail.com>
2026-02-10 20:21:02 +0700

    Modified UART option

--- app/src/retarget.c
+++ app/src/retarget.c
@@ -1,8 +1,9 @@
 #include "bsp_uart.h"
+#define PRINTF_UART BSP_UART1
 
 int _write(int fd, const void *buf, size_t count)
 {
     (void)fd;
-    BSP_UART_Write(BSP_UART1, buf, count);
+    BSP_UART_Write(PRINTF_UART, buf, count);
     return count;
 }
